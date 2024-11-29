64ad38eee974c25ce2e90c8a47802551e2372bcc
Terreturn <laisiteluo@gmail.com>
2024-11-29 14:28:59 +0100

    Update Projet pokemorpion.py

--- Projet pokemorpion/Projet pokemorpion.py
+++ Projet pokemorpion/Projet pokemorpion.py
@@ -168,38 +168,45 @@ class Pokemorpion():
 
     def random_draft(self):
         self.g.afficherImage(0,0,'distri_page.jpg',1200,600)
-        submit=self.g.afficherTexte('submit',512,512,'black')
+        submit=self.g.afficherTexte('submit',600,300,'black',25)
         self.g.actualiser()
-        pool_normal = self.pk_normal.sample(n=90)
-        pool_legend = self.pk_legend.sample(n=10)
-        pool1, pool2 = pool_legend.copy(), pool_normal.copy()
-        legned_p1 = pool1.sample(n=5, random_state=42)
-        pool1 = pool1.drop(legned_p1.index, inplace=False)
-        legned_p2 = pool1.sample(n=5, random_state=42)
-        normal_p1 = pool2.sample(n=45, random_state=42)
-        pool2 = pool2.drop(normal_p1.index, inplace=False)
-        normal_p2 = pool2.sample(n=45, random_state=42)
-        self.player_1 = pds.concat([legned_p1, normal_p1])
-        self.player_2 = pds.concat([legned_p2, normal_p2])
+        pool_1 = self.pk.loc[(self.pk['Total']<=550) & (self.pk['Total']>450)]
+        pool_2 = self.pk.loc[(self.pk['Total']<=360) & (self.pk['Total']>270)]
+        pool_3 = self.pk.loc[(self.pk['Total']<=450) & (self.pk['Total']>360)]
+        pool_4 = self.pk.loc[(self.pk['Total']>550)]
+        pl1_pool_1 = pool_1.sample(n=25, random_state=42)
+        pool_1 = pool_1.drop(pl1_pool_1.index, inplace=False)
+        pl2_pool_1 = pool_1.sample(n=25, random_state=42)
+        pl1_pool_2 = pool_2.sample(n=15, random_state=42)
+        pool_2 = pool_2.drop(pl1_pool_2.index, inplace=False)
+        pl2_pool_2 = pool_2.sample(n=15, random_state=42)
+        pl1_pool_3 = pool_3.sample(n=10, random_state=42)
+        pool_3 = pool_3.drop(pl1_pool_3.index, inplace=False)
+        pl2_pool_3 = pool_3.sample(n=10, random_state=42)
+        pl1_pool_4 = pool_4.sample(n=10, random_state=42)
+        pool_4 = pool_4.drop(pl1_pool_4.index, inplace=False)
+        pl2_pool_4 = pool_4.sample(n=10, random_state=42)
+        self.player_1 = pds.concat([pl1_pool_1,pl1_pool_2,pl1_pool_3,pl1_pool_4])
+        self.player_2 = pds.concat([pl2_pool_1,pl2_pool_2,pl2_pool_3,pl2_pool_4])
         self.player_1 = list(self.player_1.index)
         self.player_2 = list(self.player_2.index)
         #print(self.player_1)
         l1=[]
         l2=[]
-        for i in range(50):
+        for i in range(60):
             l1.append(self.get_pokemon_image(self.player_1[i]))
             l2.append(self.get_pokemon_image(self.player_2[i]))
             #print(i)
             #print(l1,l2)
-        for n in range(25):
+        for n in range(30):
             print(l1[n])
             self.g.afficherImage(10, 40*n, l1[n],40,40)
-            print(l1[n + 25])
-            self.g.afficherImage(110, 40*n, l1[n+25],40,40)
+            print(l1[n + 30])
+            self.g.afficherImage(110, 40*n, l1[n+30],40,40)
             print(l2[n])
             self.g.afficherImage(1100, 40*n, l2[n],40,40)
-            print(l2[n + 25])
-            self.g.afficherImage(1000, 40*n, l2[n+25],40,40)
+            print(l2[n + 30])
+            self.g.afficherImage(1000, 40*n, l2[n+30],40,40)
             time.sleep(0.05)
             self.g.actualiser()
         clic = self.g.attendreClic()
@@ -220,19 +227,20 @@ class Pokemorpion():
         image_2_path=self.get_pokemon_image(ch[1])
         image_1=self.g.afficherImage(200,300,image_1_path,200,200)
         image_2=self.g.afficherImage(750,300,image_2_path,200,200)
-
+        total_1=self.g.afficherTexte(f'Total: {self.pk.loc[ch[0],'Total']}',300,250,'black')
+        total_2=self.g.afficherTexte(f'Total: {self.pk.loc[ch[1],'Total']}',900,250,'black')
         while time_now <= time_start+10:
             time_now=time.time()
-            t=self.g.afficherTexte(f"time:{int(time_start+10-time_now)}", 600, 150)
+            t=self.g.afficherTexte(f"time:{int(time_start+10-time_now)}", 600, 150,'red',20)
             clic=self.g.recupererClic()
             self.g.supprimer(t)
             if clic!=None:
                 o = self.g.recupererObjet(clic.x, clic.y)
                 if o==image_1:
                     pl1.append(ch[0])
-                    print('pl1:',pl1)
                     pl2.append(ch[1])
-                    print('pl2', pl2)
+                    self.g.supprimer(total_1)
+                    self.g.supprimer(total_2)
                     ch=None
                     for i in range(10):
                         self.g.supprimer(image_1)
@@ -243,6 +251,8 @@ class Pokemorpion():
                         self.g.actualiser()
                     break
                 elif o==image_2:
+                    self.g.supprimer(total_1)
+                    self.g.supprimer(total_2)
                     pl1.append(ch[0])
                     pl2.append(ch[1])
                     ch=None
@@ -260,6 +270,8 @@ class Pokemorpion():
             if auto==1:
                 pl1.append(ch[0])
                 pl2.append(ch[1])
+                self.g.supprimer(total_1)
+                self.g.supprimer(total_2)
                 for i in range(10):
                     self.g.supprimer(image_1)
                     self.g.supprimer(image_2)
@@ -272,6 +284,8 @@ class Pokemorpion():
             else:
                 pl1.append(ch[1])
                 pl2.append(ch[0])
+                self.g.supprimer(total_1)
+                self.g.supprimer(total_2)
                 for i in range(10):
                     self.g.supprimer(image_1)
                     self.g.supprimer(image_2)
@@ -288,24 +302,38 @@ class Pokemorpion():
 
     def distribute_draft(self):
         self.g.afficherImage(0, 0, 'distri_page.jpg',1200,600)
-        pool1=self.pk_legend.sample(n=10)
-        pool2=self.pk_normal.sample(n=90)
-        for i in range(5):
+        pool1=self.pk_legend.sample(n=20)
+        pool2=self.pk_normal.sample(n=100)
+        for i in range(10):
             if i % 2 == 0:
+                t=self.g.afficherTexte('Round player 1',600,250,'black',40)
                 self.choose(pool1, self.player1, self.player2,100+i*20,10,1100-i*20,10)
+                self.g.supprimer(t)
             else:
+                t=self.g.afficherTexte('Round player 2',600,250,'black',40)
                 self.choose(pool1, self.player2, self.player1,1100-i*20,10,100+i*20,10)
-        for i in range(45):
+                self.g.supprimer(t)
+        for i in range(50):
             if i % 2 == 0:
-                self.choose(pool2, self.player1, self.player2,100,40+i*20,1100,40+i*20)
+                if i<25:
+                    t = self.g.afficherTexte('Round player 1', 600, 250, 'black', 40)
+                    self.choose(pool2, self.player1, self.player2,100,40+i*20,1100,40+i*20)
+                    self.g.supprimer(t)
+                else:
+                    t = self.g.afficherTexte('Round player 1', 600, 250, 'black', 40)
+                    self.choose(pool2, self.player1, self.player2, 50, 40 + i * 20, 1150, 40 + (i-25)* 20)
+                    self.g.supprimer(t)
             else:
-                self.choose(pool2, self.player2, self.player1,1100,40+i*20,100,40+i*20)
+                if i < 25:
+                    t = self.g.afficherTexte('Round player 2', 600, 250, 'black', 40)
+                    self.choose(pool2, self.player2, self.player1,1100,40+i*20,100,40+i*20)
+                    self.g.supprimer(t)
+                else:
+                    t = self.g.afficherTexte('Round player 2', 600, 250, 'black', 40)
+                    self.choose(pool2, self.player2, self.player1,1150,40+i*20,50,40+(i-24)*20)
+                    self.g.supprimer(t)
         print(self.player1,self.player2)
-        t_1=self.g.afficherTexte(600,500,'submit')
-        clic=self.g.attendreClic()
-        x=self.g.recupererClic(clic.x,clic.y)
-        if x==t_1:
-            self.g.fermerFenetre()
+        self.g.superclean()
 
 
 
